b0b5b152e2d49b7ec7152cd88da6e803852a9a7f
Pedro Henrique Silva Rodrigues <ph8464803@gmail.com>
2024-10-20 19:54:42 -0300

    refactor: update do BloggingRepository

--- src/main/java/com/example/blogging_platform/repository/BloggingRepository.java
+++ src/main/java/com/example/blogging_platform/repository/BloggingRepository.java
@@ -4,10 +4,11 @@ import com.example.blogging_platform.model.Blogging;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface BloggingRepository extends JpaRepository<Blogging, Integer> {
     @Query("SELECT p FROM Blogging p WHERE p.title LIKE %:term% OR p.content LIKE %:term% OR p.tags LIKE %:term%")
     List<Blogging> searchByTerm(@Param("term") String term);
